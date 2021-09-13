fd2e161706eb9d2e42120faeafd011824018a53e
fauzifadh <fauzifadhi@rollingglory.com>
2021-09-13 19:09:40 +0700

    add helper viewmodel and migration script

--- src/utils/helper/index.ts
+++ src/utils/helper/index.ts
@@ -1,2 +1,14 @@
-export const circularToJSON = (circular: unknown) =>
-  JSON.parse(JSON.stringify(circular));
+import { plainToClass } from 'class-transformer';
+
+export const circularToJSON = (circular: unknown) => JSON.parse(JSON.stringify(circular));
+
+export function generateViewModel<T, V>(cls: { new(...args: any[]): T }, obj: V[]): T[];
+export function generateViewModel<T, V>(cls: { new(...args: any[]): T }, obj: V): T;
+export function generateViewModel(...args: any[]) {
+  const result = plainToClass(args[0], circularToJSON(args[1]),
+    {
+      excludeExtraneousValues: true,
+      exposeUnsetFields: true,
+    });
+  return result as unknown;
+}
