dbd8105e283b3f5070ca034292b8348d8c541a79
fauzifadh <fauzifadhi@rollingglory.com>
2021-10-14 18:12:50 +0700

    add anonym strategy

--- src/modules/apps/auth/auth.module.ts
+++ src/modules/apps/auth/auth.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AnonymStrategy } from './strategy/anonym.strategy';
 import { AuthJwtStrategy } from './strategy/auth.strategy';
 
 @Module({
   imports: [],
-  providers: [AuthService, AuthJwtStrategy],
+  providers: [AuthService, AuthJwtStrategy, AnonymStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
--- src/modules/apps/example/controllers/user.controller.ts
+++ src/modules/apps/example/controllers/user.controller.ts
@@ -2,21 +2,20 @@ import { PermissionGuard } from '@_common/auth/guard/permissions.guard';
 import { User } from '@models/core/User';
 import { Controller, Get, Param, ParseIntPipe, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Permissions } from '@utils/decorators';
-import { PERMISSION } from '@utils/enum';
+import { User as LoggedUser } from '@utils/decorators';
 import { generateViewModel } from '@utils/helper';
 import { ResponseInterceptor } from '@utils/interceptors';
 
 import { UserViewModel } from '../viewmodel/user.viewmodel';
 
-@UseGuards(AuthGuard('auth'), PermissionGuard)
-@Permissions(PERMISSION.CAN_ADD_USER)
+@UseGuards(AuthGuard(['anonym', 'auth']), PermissionGuard)
+// @Permissions(PERMISSION.CAN_ADD_USER)
 @Controller({ version: '1', path: 'user' })
 export class UserController {
   @Get(':id')
-  @Permissions(PERMISSION.CAN_VIEW_USER)
+  // @Permissions(PERMISSION.CAN_VIEW_USER)
   @UseInterceptors(new ResponseInterceptor('users'))
-  async getUser(@Param('id', ParseIntPipe) id: string): Promise<UserViewModel> {
+  async getUser(@Param('id', ParseIntPipe) id: number, @LoggedUser() loggedUser): Promise<UserViewModel> {
     const user = await User.findOne();
 
     return generateViewModel(UserViewModel, user);
