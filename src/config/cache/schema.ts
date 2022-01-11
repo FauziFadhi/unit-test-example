a345bed8e42d389fc0aca6e83cc07e2588327c3a
Fauzi Fadhillah <fauzifadhi@gmail.com>
2022-01-11 17:07:38 +0700

    change schema env validation for cache

--- src/config/cache/schema.ts
+++ src/config/cache/schema.ts
@@ -4,6 +4,6 @@ export default Joi.object({
   CACHE_HOST: Joi.string().required(),
   CACHE_PORT: Joi.number().required().default(6379),
   CACHE_PREFIX: Joi.string().required(),
-  CACHE_TTL: Joi.number().required().default(1000),
-  CACHE_PASSWORD: Joi.string().required(),
+  CACHE_TTL: Joi.number().optional().default(5),
+  CACHE_PASSWORD: Joi.string().optional(),
 });
